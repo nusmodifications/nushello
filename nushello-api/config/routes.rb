d26f566b3001e28d7d355fa394330105acca0138
Liu Xinan <xinan@me.com>
2015-08-19 05:11:45 +0800

    Add API version constraints

--- nushello-api/config/routes.rb
+++ nushello-api/config/routes.rb
@@ -1,7 +1,9 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
