f60374207ffd44c4c0594124e9a7dd37d0fa24f0
Liu Xinan <xinan@me.com>
2015-08-19 22:31:56 +0800

    Use `delegate` instead of `has_one`

--- nushello-api/app/models/user.rb
+++ nushello-api/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   belongs_to :first_major, class_name: 'Major', foreign_key: 'first_major_id'
   belongs_to :second_major, class_name: 'Major', foreign_key: 'second_major_id'
-  has_one :faculty, through: :first_major
+
+  delegate :faculty, to: :first_major
 
   validates :facebook_id, presence: true, uniqueness: true
   validates :nusnet_id, presence: true, uniqueness: true
