27c64fab68e382f544e5e6bb626c8c1b9f5e3488
Lei Mingyu <uygnim.iel@gmail.com>
2015-08-20 04:05:35 +0800

    fix validation

--- nushello-api/app/models/conversation.rb
+++ nushello-api/app/models/conversation.rb
@@ -1,4 +1,7 @@
 class Conversation < ActiveRecord::Base
-	belongs_to :user_1, class_name: 'User', foreign_key: 'first_major_id'
-  belongs_to :user_2, class_name: 'User', foreign_key: 'second_major_id'
+	belongs_to :user_1, class_name: 'User', foreign_key: 'user_1_id'
+  belongs_to :user_2, class_name: 'User', foreign_key: 'user_2_id'
+
+  validates :user_1_id, presence: true, uniqueness: { scope => :user_2_id }
+  validates :user_2_id, presence: true, uniqueness: { scope => :user_1_id }
 end
