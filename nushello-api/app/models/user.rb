24d3042ec01f472cec0fd8ab6813e552186c794c
Liu Xinan <xinan@me.com>
2015-08-19 19:29:02 +0800

    Add validation for `User`

--- nushello-api/app/models/user.rb
+++ nushello-api/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :facebook_id, presence: true, uniqueness: true
+  validates :nusnet_id, presence: true, uniqueness: true
+  validates :name, presence: true
 end
