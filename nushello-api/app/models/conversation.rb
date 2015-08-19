50e0774e64e04f29db98a0ee88f99e29eff0c556
Lei Mingyu <uygnim.iel@gmail.com>
2015-08-20 04:02:10 +0800

    modle updated for conversations

--- nushello-api/app/models/conversation.rb
+++ nushello-api/app/models/conversation.rb
@@ -1,2 +1,4 @@
 class Conversation < ActiveRecord::Base
+	belongs_to :user_1, class_name: 'User', foreign_key: 'first_major_id'
+  belongs_to :user_2, class_name: 'User', foreign_key: 'second_major_id'
 end
--- nushello-api/app/models/user.rb
+++ nushello-api/app/models/user.rb
@@ -3,9 +3,16 @@ class User < ActiveRecord::Base
   belongs_to :second_major, class_name: 'Major', foreign_key: 'second_major_id'
   belongs_to :residence
 
+  has_many :active_conversations, class_name: 'Conversation', foreign_key: 'user_1_id'
+  has_many :passive_conversations, class_name: 'Conversation', foreign_key: 'user_2_id'
+
   delegate :faculty, to: :first_major
 
   validates :facebook_id, presence: true, uniqueness: true
   validates :nusnet_id, presence: true, uniqueness: true
   validates :name, presence: true
+
+  def conversations
+    active_conversations | passive_conversations
+  end
 end
