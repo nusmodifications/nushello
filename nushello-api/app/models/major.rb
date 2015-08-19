630889c7fcd7c4f3559dc422da3f1e368b0b3052
Liu Xinan <xinan@me.com>
2015-08-19 22:26:53 +0800

    Associate `User` and `Faculty`

--- nushello-api/app/models/major.rb
+++ nushello-api/app/models/major.rb
@@ -1,6 +1,11 @@
 class Major < ActiveRecord::Base
   belongs_to :faculty
-  has_many :users
+  has_many :home_users, class_name: 'User', foreign_key: 'first_major_id'
+  has_many :other_users, class_name: 'User', foreign_key: 'second_major_id'
 
   validates :name, presence: true, uniqueness: true
+
+  def users
+    home_users | other_users
+  end
 end
