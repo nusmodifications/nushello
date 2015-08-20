6aeb202304ddf5f91ddecd17a1e5c6e78f70cc05
Lei Mingyu <uygnim.iel@gmail.com>
2015-08-20 12:51:37 +0800

    dependent: :destroy added

--- nushello-api/app/models/user.rb
+++ nushello-api/app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   belongs_to :second_major, class_name: 'Major', foreign_key: 'second_major_id'
   belongs_to :residence
 
-  has_many :active_conversations, class_name: 'Conversation', foreign_key: 'user_1_id'
-  has_many :passive_conversations, class_name: 'Conversation', foreign_key: 'user_2_id'
+  has_many :active_conversations, class_name: 'Conversation', foreign_key: 'user_1_id', dependent: :destroy
+  has_many :passive_conversations, class_name: 'Conversation', foreign_key: 'user_2_id', dependent: :destroy
   has_many :messages, class_name: 'Message', foreign_key: 'user_id'
 
   delegate :faculty, to: :first_major
