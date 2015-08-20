2fff5e805958d85e80c39c8f59c1dc74cf3aa2fb
Liu Xinan <xinan@me.com>
2015-08-20 23:21:58 +0800

    Add `users/auth/:facebookId/:facebookToken` endpoint

--- nushello-api/config/routes.rb
+++ nushello-api/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      get 'users/auth/:facebookId/:facebookToken', to: 'users#auth'
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
