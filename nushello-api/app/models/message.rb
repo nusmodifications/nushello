4337cacef0128227532c9e88174a8e3775a88dfe
Liu Xinan <xinan@me.com>
2015-08-20 19:11:35 +0800

    Touch `Conversation` when there is new messages

--- nushello-api/app/models/message.rb
+++ nushello-api/app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
   belongs_to :user
-  belongs_to :conversation
+  belongs_to :conversation, touch: true
 
   validates :content, presence: true
   validates :conversation_id, presence: true
