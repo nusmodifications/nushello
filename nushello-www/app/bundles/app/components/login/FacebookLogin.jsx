553c1f63403f28eff3f241ce3b0f77e19aa5c4af
Lei Mingyu <uygnim.iel@gmail.com>
2015-08-21 17:31:15 +0800

    bind with fb app

--- nushello-www/app/bundles/app/components/login/FacebookLogin.jsx
+++ nushello-www/app/bundles/app/components/login/FacebookLogin.jsx
@@ -6,13 +6,46 @@ export default class FacebookLogin extends React.Component {
 
   constructor(props, context) {
     super(props, context);
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
+  componentDidMount() {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1467581460234203',
+        xfbml      : true,
+        version    : 'v2.4'
+      });
+    };
+
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
+  handleClick() {
+    FB.login(this.handleLogin);
+  }
+
+  handleLogin(response) {
+    if (response.status === "connected") {
+      var accessToken = response.authResponse.accessToken;
+      var userID = response.authResponse.userID;
+      // api here
+    } else {
+      // error message here
+    }
+  }
 
   render() {
 
     return (
-        <button className='facebook-login' onClick="">
+        <button className='facebook-login' onClick={this.handleClick}>
           Log in using Facebook!
         </button>
     );
