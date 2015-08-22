76e22a620c95d07bccc5aa55d692fa3359f140ff
Liu Xinan <xinan@me.com>
2015-08-22 17:34:12 +0800

    Add presence validation for all fields in `Personality`

--- nushello-api/app/models/personality.rb
+++ nushello-api/app/models/personality.rb
@@ -1,8 +1,8 @@
 class Personality < ActiveRecord::Base
   belongs_to :user
 
-  validates :party, inclusion: { in: [true, false] }, allow_nil: true
-  validates :sports, inclusion: { in: [true, false] }, allow_nil: true
-  validates :mugger, inclusion: { in: [true, false] }, allow_nil: true
-  validates :introvert, inclusion: { in: [true, false] }, allow_nil: true
+  validates :party, presence: true, inclusion: { in: [true, false] }, allow_nil: true
+  validates :sports, presence: true, inclusion: { in: [true, false] }, allow_nil: true
+  validates :mugger, presence: true, inclusion: { in: [true, false] }, allow_nil: true
+  validates :introvert, presence: true, inclusion: { in: [true, false] }, allow_nil: true
 end
