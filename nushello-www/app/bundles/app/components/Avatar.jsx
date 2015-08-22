0b0918ab97074936a84a420e49ccf42fcce928ae
Karen Ang <thekarenang@gmail.com>
2015-08-22 15:36:57 +0800

    Add `avatar` component

---
+++ nushello-www/app/bundles/app/components/Avatar.jsx
@@ -0,0 +1,15 @@
+import React  from 'react';
+
+export default class How extends React.Component {
+
+
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <span><img className="profile-pic" src="red_panda.jpg" /></span>
+    );
+  }
+}
