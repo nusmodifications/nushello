cceaf9cbb796f133bdf30916d3e125dddc66ef7e
Liu Xinan <xinan@me.com>
2015-08-25 03:00:50 +0800

    Filter rejected conversations

--- nushello-api/app/models/user.rb
+++ nushello-api/app/models/user.rb
@@ -5,8 +5,10 @@ class User < ActiveRecord::Base
 
   has_one :personality, dependent: :destroy, autosave: true
   has_one :preference, dependent: :destroy, autosave: true
-  has_many :active_conversations, class_name: 'Conversation', foreign_key: 'user_1_id', dependent: :destroy
-  has_many :passive_conversations, class_name: 'Conversation', foreign_key: 'user_2_id', dependent: :destroy
+  has_many :active_conversations, -> { where.not(conversations: { user_1_status: 2, user_2_status: 2 }) },
+      class_name: 'Conversation', foreign_key: 'user_1_id', dependent: :destroy
+  has_many :passive_conversations, -> { where.not(conversations: { user_1_status: 2, user_2_status: 2 }) },
+      class_name: 'Conversation', foreign_key: 'user_2_id', dependent: :destroy
   has_many :messages, class_name: 'Message', foreign_key: 'user_id'
 
   delegate :faculty, to: :first_major, allow_nil: true
