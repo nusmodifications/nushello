45a98454ee8f26edb97fb04c97b20e5f5dba0365
Liu Xinan <xinan@me.com>
2015-08-25 02:51:56 +0800

    Add `FriendSerializer`

---
+++ nushello-api/app/serializers/friend_serializer.rb
@@ -0,0 +1,22 @@
+class FriendSerializer < ActiveModel::Serializer
+  attributes :facebook_id, :name, :last_name, :fake_name, :gender, :matriculation_year, :faculty,
+             :first_major, :second_major, :residence, :online, :profile_picture_url, :fake_profile_picture_url
+
+  has_one :personality, serializer: PersonalitySerializer
+
+  def faculty
+    object.faculty.try(:name)
+  end
+
+  def first_major
+    object.first_major.try(:name)
+  end
+
+  def second_major
+    object.second_major.try(:name)
+  end
+
+  def residence
+    object.residence.try(:name)
+  end
+end
