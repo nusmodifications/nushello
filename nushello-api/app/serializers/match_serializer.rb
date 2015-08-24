87e37fe6bd13293eefd251a6527dba893e986c68
Liu Xinan <xinan@me.com>
2015-08-24 13:05:11 +0800

    Add `MatchSerializer`

---
+++ nushello-api/app/serializers/match_serializer.rb
@@ -0,0 +1,21 @@
+class MatchSerializer < ActiveModel::Serializer
+  attributes :fake_name, :gender, :matriculation_year, :faculty, :first_major, :second_major, :residence, :online
+
+  has_one :personality, serializer: PersonalitySerializer
+
+  def faculty
+    object.faculty.try(:name)
+  end
+
+  def first_major
+    object.first_major.try(:name)
+  end
+
+  def second_major
+    object.second_major.try(:name)
+  end
+
+  def residence
+    object.residence.try(:name)
+  end
+end
