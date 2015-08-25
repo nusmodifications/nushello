c38c2cb2a9b64bcafc25c195c00b410574379be9
Karen Ang <thekarenang@gmail.com>
2015-08-25 16:03:11 +0800

    Add ActionTypes Constants for messages

---
+++ nushello-www/app/bundles/app/constants/Chat/MessageConstants.jsx
@@ -0,0 +1,8 @@
+// ACTION TYPE: new message was POSTed to API
+export const = MESSAGE_ADD_REQUESTED = 'MESSAGE_ADD_REQUESTED';
+
+// ACTION TYPE: new message was successfully created
+export const = MESSAGE_ADD_SUCCESS = 'MESSAGE_ADD_SUCCESS';
+
+// ACTION TYPE: new message was rejected by API
+export const = MESSAGE_ADD_FAILED = 'MESSAGE_ADD_FAILED';
