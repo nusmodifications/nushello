db054d315203dcaf546a3e8301a2b065bb3122a4
Bili Xu <xubili93@gmail.com>
2015-08-28 21:01:23 +0800

    Add profile page layout

--- nushello-www-new/src/js/views/profile-page/profile-page.jsx
+++ nushello-www-new/src/js/views/profile-page/profile-page.jsx
@@ -1,5 +1,6 @@
 'use strict';
 import React from 'react';
+import Avatar from '../../components/avatar/avatar.jsx';
 
 require('./profile-page.scss');
 
@@ -12,11 +13,46 @@ export default class ProfilePage extends React.Component {
   render() {
     return (
       <div>
-        <div className="container profile">
+        <div className="container-fluid profile">
           <div className="row">
-            <div className="col-sm-6 col-sm-offset-1">
-              <h1>User Profile</h1>
+
+            <div className="col-md-3 text-center">
+              <Avatar />
+              <h2>Xu Bili</h2>
+              <p><em>I love my life</em></p>
+              <button className="btn btn-default">
+                Edit Bio
+              </button>
             </div>
+
+            <div className="col-md-9">
+              <div className="col-md-12">
+                <div className="notifications">
+                  <h2>Notifications</h2>
+                  <hr/>
+                  <ul>
+                    <li>Fanli liked you!</li>
+                    <li>Fanli liked you!</li>
+                    <li>Fanli liked you!</li>
+                    <li>Fanli liked you!</li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="col-md-12">
+                <div className="activities">
+                  <h2>Latest Activites</h2>
+                  <hr/>
+                  <ul>
+                    <li>Fanli liked you!</li>
+                    <li>Fanli liked you!</li>
+                    <li>Fanli liked you!</li>
+                    <li>Fanli liked you!</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
