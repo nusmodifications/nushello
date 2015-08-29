f1b62415146a10b4c5717952047f1876e9cb8ca8
Karen Ang <thekarenang@gmail.com>
2015-08-30 02:36:28 +0800

    Styling for about and privacy page; tweak styling for login page

--- nushello-www-new/src/js/views/about-page/about-page.jsx
+++ nushello-www-new/src/js/views/about-page/about-page.jsx
@@ -20,21 +20,37 @@ class AboutPage extends React.Component {
 
   render() {
     return (
-      <section id="about">
-        <h1>About</h1>
-          NUSHello is anonymous, real-time chatting made exclusively for the
-          NUS community to discover and make new friends.
-          <h2>Why NUSHello?</h2>
-            You might find it difficult to make new friends (platonic or not) without joining
-             numerous CCAs and attending orientation camps. In lectures and tutorials, you
-             find a convenient seat at the corner, or otherwise leave the appropriate
-             one-chair-away-distance from strangers you do not know. Other social networking
-             apps either consist of people you already know (bah, the same old stories) or
-             are too full of noise (Tinder? NOPE!). So you go instead to NUSWhispers,
-             anonymously seeking some solution. NUSHello is that solution you have been
-             searching for.
+      <div>
+        <div className="row about-wrapper">
+          <div className="col-sm-6 col-sm-offset-3">
+            <div className="card">
+              <div className="header">
+                <h2 className="title" id="title">About</h2>
+                </div>
+              <div className="content">
+                <p>NUSHello is anonymous, real-time chatting made exclusively for the
+                NUS community to discover and make new friends.</p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="header">
+                  <h2 className="title" id="title">Why NUSHello?</h2>
+                  </div>
+                <div className="content">
+                  <p>You might find it difficult to make new friends (platonic or not) without joining
+                  numerous CCAs and attending orientation camps. In lectures and tutorials, you
+                  find a convenient seat at the corner, or otherwise leave the appropriate
+                  one-chair-away-distance from strangers you do not know. Other social networking
+                  apps either consist of people you already know (bah, the same old stories) or
+                  are too full of noise (Tinder? NOPE!). So you go instead to NUSWhispers,
+                  anonymously seeking some solution.</p>
+                  <p>NUSHello is that solution you have been searching for.</p>
+                 </div>
+               </div>
+            </div>
+          </div>
         <Footer/>
-      </section>
+      </div>
     );
   }
 }
