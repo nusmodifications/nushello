39f09245ccec0a1caf665f0d9c0811ee07d11984
Karen Ang <thekarenang@gmail.com>
2015-08-30 03:14:15 +0800

    Fix queries for matching 💘

--- nushello-api/app/controllers/api/v1/matches_controller.rb
+++ nushello-api/app/controllers/api/v1/matches_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::MatchesController < ApplicationController
   def search
     preference = @user.preference
-    @possibilities = User.joins(:personality).where('faculty = ? AND major =? AND gender = ? AND NOT id = ?', preference.faculty,
-      preference.major, preference.gender, @user.id)
+    faculty, major, gender = preference.get_filters
+    @possibilities = User.includes(:first_major, :personality).where('majors.faculty_id = ?', faculty) unless faculty.nil?
+    @possibilities = @possibilities.where(gender: gender) unless gender.nil?
+    @possibilities = @possibilities.where(first_major: major) unless major.nil?
 
     matches = ActiveModel::ArraySerializer.new(matches(preference), each_serializer: MatchSerializer)
     generate_api_payload('matches', matches)
@@ -14,10 +16,10 @@ class Api::V1::MatchesController < ApplicationController
     party, sports, mugger, introvert = preference.get_desired_personality
     result = @possibilities.where('party = ? AND sports = ? AND mugger = ? AND introvert = ?', party, sports, mugger, introvert)
     return result unless result.empty?
-    not_party = @possibilities.where(sports: sports).where(mugger: mugger).where(introvert: introvert)
-    not_sports = @possibilities.where(party: party).where(mugger: mugger).where(introvert: introvert)
-    not_mugger = @possibilities.where(party: party).where(sports: sports).where(introvert: introvert)
-    not_introvert = @possibilities.where(party: party).where(sports: sports).where(mugger: mugger)
+    not_party = @possibilities.where(personalities: { sports: sports, mugger: mugger, introvert: introvert })
+    not_sports = @possibilities.where(personalities: { party: party, mugger: mugger, introvert: introvert })
+    not_mugger = @possibilities.where(personalities: { party: party, sports: sports, introvert: introvert })
+    not_introvert = @possibilities.where(personalities: { party: party, sports: sports, mugger: mugger })
     not_party | not_sports | not_mugger | not_introvert
   end
 end
