52792ff2366b774cbd83666c0fb4d4f28e03ac1e
Liu Xinan <xinan@me.com>
2015-08-30 01:50:18 +0800

    Remove unnecessary commented codes

--- nushello-www-new/src/js/stores/auth-store.js
+++ nushello-www-new/src/js/stores/auth-store.js
@@ -17,40 +17,6 @@ var AuthStore = Reflux.createStore({
     if (response) {
       currentUser = response;
 
-      // //
-      // currentUser.permittedPages = _.pluck(currentUser.pages, 'id');
-      // currentUser.permittedPages.push('dashboard');
-
-      // // user locale setting
-      // if (currentUser.locale) {
-      //   var localeMap = {
-      //     'en': 'en',
-      //     'en_my': 'en',
-      //     'en_sg': 'en',
-      //     'en_us': 'en',
-      //     'en_vn': 'en',
-      //     'en_id': 'en',
-      //     'th_th': 'th',
-      //     'th': 'th'
-      //   };
-      //   currentUser.locale = localeMap[currentUser.locale];
-      // } else {
-      //   currentUser.locale = 'en';
-      // }
-
-      // // set user location
-      // if (currentUser.city_latitude) {
-      //   currentUser.latitude = currentUser.city_latitude;
-      //   currentUser.longitude = currentUser.city_longitude;
-      // } else if (currentUser.country_hq_city_latitude) {
-      //   currentUser.latitude = currentUser.country_hq_city_latitude;
-      //   currentUser.longitude = currentUser.country_hq_city_longitude;
-      // } else {
-      //   currentUser.latitude = 3.10820;
-      //   currentUser.longitude = 101.64012;
-      // }
-
-
       this.trigger({
         currentUser: currentUser
       });
