6eff4eb517e163827a24eb35baaf9c36801aa4da
Karen Ang <thekarenang@gmail.com>
2015-08-29 23:14:50 +0800

    Add content for about page

--- nushello-www-new/src/js/views/about-page/about-page.jsx
+++ nushello-www-new/src/js/views/about-page/about-page.jsx
@@ -21,7 +21,18 @@ class AboutPage extends React.Component {
   render() {
     return (
       <section id="about">
-        <strong>About</strong> page here!
+        <h1>About</h1>
+          NUSHello is anonymous, real-time chatting made exclusively for the
+          NUS community to discover and make new friends.
+          <h2>Why NUSHello?</h2>
+            You might find it difficult to make new friends (platonic or not) without joining
+             numerous CCAs and attending orientation camps. In lectures and tutorials, you 
+             find a convenient seat at the corner, or otherwise leave the appropriate
+             one-chair-away-distance from strangers you do not know. Other social networking
+             apps either consist of people you already know (bah, the same old stories) or 
+             are too full of noise (Tinder? NOPE!). So you go instead to NUSWhispers, 
+             anonymously seeking some solution. NUSHello is that solution you have been 
+             searching for.
         <Footer/>
       </section>
     );
