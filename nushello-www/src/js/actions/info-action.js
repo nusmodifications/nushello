a6947f25bcf215f99de4943cc49318bb7790bb59
Lei Mingyu <uygnim.iel@gmail.com>
2015-09-05 19:43:49 +0800

    separate preference to a new component

--- nushello-www/src/js/actions/info-action.js
+++ nushello-www/src/js/actions/info-action.js
@@ -4,16 +4,6 @@ import Reflux from 'reflux';
 import ProfileAPI from 'utils/api/profile-api';
 
 var InfoAction = Reflux.createActions({
-  'init': {asyncResult: true},
-  'edit': {asyncResult: true}
-});
-
-InfoAction.init.listenAndPromise(function() {
-  return ProfileAPI.init();
-});
-
-InfoAction.edit.listenAndPromise(function(data) {
-  return ProfileAPI.editAll(data);
 });
 
 export default InfoAction;
--- nushello-www/src/js/stores/info-store.js
+++ nushello-www/src/js/stores/info-store.js
@@ -4,43 +4,12 @@ import Reflux from 'reflux';
 import cookie from 'react-cookie';
 import InfoAction from 'actions/info-action';
 import PermissionStore from 'stores/permission-store';
-import FacultyPickerStore from 'stores/faculty-picker-store';
 
 var InfoStore = Reflux.createStore({
   listenables: [InfoAction],
 
   init: function() {
     this.listenTo(PermissionStore, this.updatePermission);
-    this.listenTo(FacultyPickerStore, this.updateSelectedFaculty);
-  },
-
-  onInit: function(res) {
-  },
-
-  onInitCompleted: function(res) {
-    if (res.type === 'userProfile') {
-      this.trigger({
-        profile: res.data
-      });
-    }
-  },
-
-  onInitFailed: function(msg) {
-    this.trigger(msg);
-  },
-
-  onEdit: function(res) {
-
-  },
-
-  onEditCompleted: function(res) {
-    this.trigger({
-      goMatch: true
-    });
-  },
-
-  onEditFailed: function(res) {
-    this.trigger(res);
   },
 
   updatePermission: function(res) {
@@ -49,12 +18,7 @@ var InfoStore = Reflux.createStore({
         canGo: res.canGo
       });
     }
-  },
-
-  updateSelectedFaculty: function(res) {
-    this.trigger(res);
   }
-
 });
 
 export default InfoStore;
