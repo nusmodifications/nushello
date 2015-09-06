57c44b4732bd5f7b7dacc93c360a801328c577fd
Liu Xinan <xinan@me.com>
2015-09-07 04:08:29 +0800

    Notify Slack if for IVLE auto seeding

--- nushello-api/app/models/faculty.rb
+++ nushello-api/app/models/faculty.rb
@@ -1,6 +1,16 @@
 class Faculty < ActiveRecord::Base
+  include HTTParty
+
   has_many :majors, dependent: :destroy
   has_many :users, through: :majors, source: :home_users
 
   validates :name, presence: true, uniqueness: true
+
+  before_create :notify_slack, if: proc { |m| Rails.env.production? }
+
+  def notify_slack
+    self.class.post(ENV['SLACK_WEBHOOK_URL'], body: {
+      text: "New faculty created: #{name}"
+    }.to_json)
+  end
 end
--- nushello-api/app/models/major.rb
+++ nushello-api/app/models/major.rb
@@ -1,11 +1,21 @@
 class Major < ActiveRecord::Base
+  include HTTParty
+
   belongs_to :faculty
   has_many :home_users, class_name: 'User', foreign_key: 'first_major_id'
   has_many :other_users, class_name: 'User', foreign_key: 'second_major_id'
 
   validates :name, presence: true, uniqueness: true
 
+  before_create :notify_slack, if: proc { |m| Rails.env.production? }
+
   def users
     home_users | other_users
+  end
+
+  def notify_slack
+    self.class.post(ENV['SLACK_WEBHOOK_URL'], body: {
+      text: "New major created: #{name}"
+    }.to_json)
   end
 end
