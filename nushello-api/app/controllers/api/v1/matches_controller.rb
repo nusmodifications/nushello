47189d8fffe8b9a0724f2459a499143eb5bbf255
Liu Xinan <xinan@me.com>
2015-09-09 22:42:59 +0800

    Do not join personalities table

--- nushello-api/app/controllers/api/v1/matches_controller.rb
+++ nushello-api/app/controllers/api/v1/matches_controller.rb
@@ -22,7 +22,7 @@ class Api::V1::MatchesController < ApplicationController
     preference = @user.preference
     return generate_api_payload('matches', random) unless preference.present?
     faculty, major, gender = preference.get_filters
-    @possibilities = User.joins(:first_major, :personality).where.not(id: @user)
+    @possibilities = User.joins(:first_major).where.not(id: @user)
     @possibilities = @possibilities.where('majors.faculty_id = ?', faculty) unless faculty.nil?
     @possibilities = @possibilities.where(gender: gender) unless gender.nil?
     @possibilities = @possibilities.where(first_major: major) unless major.nil?
