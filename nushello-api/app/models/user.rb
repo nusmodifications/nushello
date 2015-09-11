d14df0e3aa4a2b6ee82ac362f8f8ff2cbf764e0d
Liu Xinan <xinan@me.com>
2015-09-11 11:26:14 +0800

    Fix user create

--- nushello-api/app/models/user.rb
+++ nushello-api/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include HTTParty
+
   belongs_to :first_major, class_name: 'Major', foreign_key: 'first_major_id'
   belongs_to :second_major, class_name: 'Major', foreign_key: 'second_major_id'
   belongs_to :residence
